8eb7e9aa7c89646132304be423f0c9f1fdde3153
Wilder Rodrigues <wilder.rodrigues@gmail.com>
2021-05-28 11:16:20 +0200

    Improve unit test; rename dataset items in dictionary.

--- clip_mania/utils/data/preprocess.py
+++ clip_mania/utils/data/preprocess.py
@@ -5,12 +5,12 @@ class DatasetProcessor:
 
     @staticmethod
     def create_dataset(dataset_path: str):
-        dataset = {'item': []}
+        dataset = {'items': []}
         for root, dirs, _ in os.walk(dataset_path):
             for label in dirs:
                 for _, _, sub_files in os.walk(os.path.join(root, label)):
                     for file in sub_files:
-                        dataset['item'].append((os.path.join(root, label, file),
+                        dataset['items'].append((os.path.join(root, label, file),
                                                 f'This is a picture of a {label}.'))
 
         return dataset
--- test/test_dataset/test_preprocess.py
+++ test/test_dataset/test_preprocess.py
@@ -1,11 +1,20 @@
+import os
+
 from unittest import TestCase
+
+from pathlib import Path
 
 from clip_mania.utils.data.preprocess import DatasetProcessor
 
 
 class TestDatasetProcessor(TestCase):
 
+    def setUp(self):
+        self.current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+
     def test_create_dataset(self):
-        dataset = DatasetProcessor.create_dataset('./dataset/train')
+        dataset_path = os.path.join(self.current_path, 'dataset/train')
+        dataset = DatasetProcessor.create_dataset(dataset_path)
         self.assertIsNotNone(dataset)
         self.assertTrue(len(dataset) == 1)
+        self.assertTrue(len(dataset['items']) == 6)
