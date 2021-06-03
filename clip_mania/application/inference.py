8003b0ae2d8eaa3e469c522269553d5fec89ef30
Wilder Rodrigues <wilder.rodrigues@gmail.com>
2021-06-03 08:25:02 +0200

    Create Pandas Dataframe with inference results.

--- clip_mania/application/inference.py
+++ clip_mania/application/inference.py
@@ -1,8 +1,11 @@
 import os
 import glob
+import re
 
 import PIL
 import numpy as np
+
+import pandas as pd
 
 import torch
 import clip
@@ -33,18 +36,28 @@ def main(_args):
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model, preprocess = clip.load("ViT-B/32", device=device, jit=False)
+
     checkpoint = torch.load(model_path)
     model.load_state_dict(checkpoint['model_state_dict'])
     model = model.eval().to(device)
 
     indexed_prompts = DatasetProcessor.create_indexed_prompts(test_dataset_path)
     prompts = list(indexed_prompts.keys())
-
     text = clip.tokenize(prompts).to(device)
+
     full_path = os.path.join(test_dataset_path, "**/*.jpg")
     images = glob.glob(full_path, recursive=True)
+
+    expected_pattern = r"(?<=test/)((?!=\D)).+(?=/(?=[\d|\D]))"
+    regex_e = re.compile(expected_pattern)
+    predicted_pattern = r"(?<=\)\s).*[^\.]"
+    regex_p = re.compile(predicted_pattern)
+
+    results = {}
     for image_path in images:
         image = preprocess(PIL.Image.open(image_path)).unsqueeze(0).to(device)
+        expected_class = "_".join([regex_e.search(image_path).group(), "y"])
+
         with torch.no_grad():
             _image_features = model.encode_image(image).to(device)
             _text_features = model.encode_text(text).to(device)
@@ -54,11 +67,21 @@ def main(_args):
 
             max_index = np.argmax(probs)
             prediction = prompts[max_index]
+            predicted_class = "_".join([regex_p.search(prediction).group(), "y_hat"])
+
             prob = probs.flatten()[max_index]
 
-            logging.info(f"Image path: {image_path}")
-            logging.info(f"Max probability is: {prob}")
-            logging.info(f"Predicted class is: {prediction}")
+            if expected_class in results:
+                results[expected_class].append(1)
+            else:
+                results[expected_class] = [1]
+            if predicted_class in results:
+                results[predicted_class].append(prob)
+            else:
+                results[predicted_class] = [prob]
+
+    results_df = pd.DataFrame.from_dict(data=results)
+    results_df.to_csv("inference_results.csv", index=False)
 
 
 if __name__ == '__main__':
